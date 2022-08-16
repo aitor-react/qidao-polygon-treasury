b76ce69211135483183b3175b20a463a8f5ea1bc
aitor <penguinft1@gmail.com>
2022-08-16 16:33:34 +0200

    add files, set up react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+//import { getTransfers } from './utils/getTransfersAPI' 
+//import { Line } from 'react-chartjs-2'
+//import axios from 'axios'
+
+
+export default function App() {
+
+    //const [chartData, setChartData] = useState([])
+
+    
+
+  return (
+    <>
+      
+      
+      
+      App
+      
+
+    </>
+  )
+}
--- src/utils/getTransfersAPI.js
+++ src/utils/getTransfersAPI.js
@@ -1,12 +1,9 @@
+import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
-import axios from "axios";
-
-  const apiKey = process.env.apiKey;
-  const apiUrl = "https://polygon-mainnet.g.alchemyapi.io/v2/"
 
   var data = JSON.stringify({
-      id: 0,
+      id: 1,
       jsonrpc: '2.0',
       method: 'alchemy_getAssetTransfers',
       params: [
@@ -17,14 +14,14 @@ import axios from "axios";
           withMetadata: true,
           excludeZeroValue: false,
           maxCount: '0x3e8',
-          fromAddress: '0xAd95A5fE898679B927C266eB2eDfAbC7fe268C27'
+          fromAddress: "0xAd95A5fE898679B927C266eB2eDfAbC7fe268C27"
         }
       ]
     })
 
   var config = {
-    method: 'post',
-    url: `${apiUrl}${apiKey}`,
+    method: 'POST',
+    url: process.env.API_KEY,
     headers: {
       'Content-Type': 'application/json'
     },
