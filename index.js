353a8f234cb6bf36a755a1b91d80338fe613943e
aitor <penguinft1@gmail.com>
2022-08-16 02:30:59 +0200

    refactor axios script

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-import fetch from "node-fetch";
-import 'dotenv/config'
+import dotenv from "dotenv";
+dotenv.config();
+import axios from "axios";
 
-const apiKey = process.env.apiKey;
+  const apiKey = process.env.apiKey;
+  const apiUrl = "https://polygon-mainnet.g.alchemyapi.io/v2/"
 
-const options = {
-  method: 'POST',
-  headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-  body: JSON.stringify({
-    id: 1,
-    jsonrpc: '2.0',
-    method: 'alchemy_getAssetTransfers',
-    params: [
-      {
-        fromBlock: '0x0',
-        toBlock: 'latest',
-        category: ['erc20'],
-        withMetadata: true,
-        excludeZeroValue: false,
-        maxCount: '0x3e8',
-        fromAddress: '0xAd95A5fE898679B927C266eB2eDfAbC7fe268C27'
-      }
-    ]
-  })
-};
+  var data = JSON.stringify({
+      id: 0,
+      jsonrpc: '2.0',
+      method: 'alchemy_getAssetTransfers',
+      params: [
+        {
+          fromBlock: '0x0',
+          toBlock: 'latest',
+          category: ['erc20'],
+          withMetadata: true,
+          excludeZeroValue: false,
+          maxCount: '0x3e8',
+          fromAddress: '0xAd95A5fE898679B927C266eB2eDfAbC7fe268C27'
+        }
+      ]
+    })
 
-fetch(`https://polygon-mainnet.g.alchemyapi.io/v2/${apiKey}`, options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+  var config = {
+    method: 'post',
+    url: `${apiUrl}${apiKey}`,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+
+  axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
